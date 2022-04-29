4690231c3b655eb061c50d393c2a304ab58f7e8d
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-04-29 22:30:11 +0200

    more component, create accordian

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,5 +1,41 @@
-import { Container } from '@mui/material';
+import { Box, Container } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import ImageCollage from '../components/ImageCollage';
+import CustomizedAccordions from '../components/Accordian';
 
-const Tour = () => <Container sx={{ width: 900 }}>Hello</Container>;
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant='3' component='h1' mt={3}>
+      Explore the World in Kigali
+    </Typography>
+    <Box marginTop={3} sx={{ display: 'flex' }}>
+      <img
+        src='https://cdn.getyourguide.com/img/tour/5eba9b246bb9a.jpeg/145.jpg'
+        alt=''
+        height={325}
+      />
+      <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant='h6' component='h4' mt={3}>
+        Why you should visit Kigali?
+      </Typography>
+      <Typography variant='paragraph' component='p' mt={3}>
+        Kigali is clean, safe and filled with enlightening cultural experiences
+        from world-class modern art galleries to fashion, local crafts and even
+        a coffee co-op run by women making the world's best coffee. Who should
+        go? Adventurous, curious, eco-conscious travelers and conservationists.
+      </Typography>
+    </Box>
+
+    <Box>
+      <Typography variant='h6' component='h4' mt={3} mb={2}>
+        Frequently Asked Questions about Kigali and Is Kigali a good place to
+        live?
+      </Typography>
+      <CustomizedAccordions />
+    </Box>
+  </Container>
+);
 
 export default Tour;
