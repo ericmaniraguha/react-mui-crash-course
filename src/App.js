e373fb46536a352c5df35b2a3df9a27933e54b86
Eric Maniraguha <ericmaniraguha@gmail.com>
2022-04-29 20:30:57 +0200

    route before destructuring

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
+import Home from './pages/Home';
 import './App.css';
 
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import SearchAppBar from './components/AppBar';
-import TourCard from './components/TourCard';
-import cities from './data.json';
-import { Typography } from '@mui/material';
-
 function App() {
-  return (
-    <div className='App'>
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map((city) => (
-          <>
-            <Typography
-              variant='h4'
-              component='h2'
-              marginTop={5}
-              marginBottom={3}
-            >
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={5}>
-              {city.tours.map((tour, index) => (
-                <TourCard tour={tour} key={index} />
-              ))}
-            </Grid>
-          </>
-        ))}
-      </Container>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
